4b47b78fd69a314d182bea4ec53191712d1f6f94
MarcBM <marc.betbeder@hotmail.com>
2025-05-16 16:18:11 +1000

    2.3 FPS Capped at 60

--- main.py
+++ main.py
@@ -4,18 +4,20 @@ from constants import *
 def main():
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-  game_loop(screen)
+  clock = pygame.time.Clock()
+  dt = 0
   
-def game_loop(screen):
-    while True:
-      for event in pygame.event.get():
-          if event.type == pygame.QUIT:
-              return
-      
-      screen.fill((0, 0, 0))  # Fill the screen with black
-      
-      
-      pygame.display.flip()  # Update the display - LAST STEP
+  # Game Loop
+  while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return
+    
+    screen.fill((0, 0, 0))  # Fill the screen with black
+    
+    
+    pygame.display.flip()  # Update the display - LAST DRAW STEP
+    dt = clock.tick(60)  # Limit the frame rate to 60 FPS
       
 
 if __name__ == "__main__":
