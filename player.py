b31ce0cb3dbe82e072995e169d2779aefc3894bb
MarcBM <marc.betbeder@hotmail.com>
2025-05-16 17:01:53 +1000

    3.4 - Shoot Rate

--- player.py
+++ player.py
@@ -1,14 +1,17 @@
 import pygame
 from circleshape import CircleShape
 from shot import Shot
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN
 
 class Player(CircleShape):
   def __init__(self, x, y):
     super().__init__(x, y, PLAYER_RADIUS)
     self.rotation = 0
+    self.shoot_timer = 0.0
     
   def update(self, dt):
+    self.shoot_timer -= dt
+    
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_a]:
@@ -31,9 +34,13 @@ class Player(CircleShape):
     
   def shoot(self):
     # create a new shot
-    shot = Shot(self.position.x, self.position.y)
-    shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
-    return shot
+    if self.shoot_timer <= 0:
+      self.shoot_timer = PLAYER_SHOOT_COOLDOWN
+      shot = Shot(self.position.x, self.position.y)
+      shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+      return shot
+    else:
+      return None
     
   def draw(self, screen):
     pygame.draw.polygon(screen, (255, 255, 255), self.triangle(), 2)
